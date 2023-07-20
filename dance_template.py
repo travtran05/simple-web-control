6e884462351a84d19e5aca6fc9d642a41354476d
travistran <travistran544@gmail.com>
2023-07-20 16:22:12 -0400

    turn

--- dance_template.py
+++ dance_template.py
@@ -29,21 +29,23 @@ def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None
     :param motor_settings: The motor settings, a list of 6 values -100 to 100
     :return: None
     """
-    step = 0
-    while step < seconds:
+    start_time = time.time()
+    while time.time()-start_time < seconds:
         for i in range(len(motor_settings)):
             test_motor(mav_connection=mav_connection, motor_id=i, power=motor_settings[i])
         time.sleep(0.2)
-        step += 0.2
 
 if __name__ == "__main__":
     ####
     # Initialize ROV
     ####
     mav_connection = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+    print("Waiting for connection")
     mav_connection.wait_heartbeat()
     # Arm the ROV and wait for confirmation
     arm_rov(mav_connection)
+
+    print("ARMED")
 
     ####
     # Run choreography
@@ -52,12 +54,27 @@ if __name__ == "__main__":
     Call sequence of calls to run_timed_motors to execute choreography
     Motors power ranges from -100 to 100
     """
-    run_motors_timed(mav_connection, seconds=5, motor_settings=[100,-100 ,100 ,-100, 0, 0])
-    run_motors_timed(mav_connection, seconds=5, motor_settings=[-100,100 ,-100 ,100, 0, 0])
+    
+   
+    for i in range(10):
+        run_motors_timed(mav_connection, seconds=3.75, motor_settings=[-100,-100 ,0 ,100, 0, 0])
+        print("command one")
+        run_motors_timed(mav_connection, seconds=3.75, motor_settings=[100,100,0,100, 0, 0])
+        print("command two")
+
+        run_motors_timed(mav_connection, seconds=3.75, motor_settings=[100,0 ,-100 ,-100, 0, 0])
+        print("command three")
+        run_motors_timed(mav_connection, seconds=3.75, motor_settings=[100,0, 100,100, 0, 0])
+        print("command four")
+
+
+
+
     # stop
     run_motors_timed(mav_connection, seconds=5, motor_settings=[0, 0, 0, 0, 0, 0])
-
+    print("stopped")
     ####
     # Disarm ROV and exit
     ####
     disarm_rov(mav_connection)
+    print("disarmed")
